dec535b74272d41ba12858099dacde1fee143544
Greg RV <gregvarias@gmail.com>
2015-03-27 13:32:46 -0700

    make api call to rotten tomatoes and render data

--- index.ios.js
+++ index.ios.js
@@ -17,22 +17,67 @@ var MOCKED_MOVIES_DATA = [
     {title: 'Half Baked', year: '1998', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ]
 
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL =
+'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
 var AwesomeProject = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
-    return (
-        <View style={styles.container}>
-            <Image
-                source={{uri: movie.posters.thumbnail}}
-                style={styles.thumbnail}
-            />
-            <View style={styles.rightContainer}>
-                <Text style={styles.title}>{movie.title}</Text>
-                <Text style={styles.year}>{movie.year}</Text>
+    getInitialState: function(){
+        return {
+            movies: null,
+        };
+    },
+
+    componentDidMount: function() {
+        this.fetchData();
+    },
+
+    fetchData: function() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    },
+
+    render: function() {
+    if (!this.state.movies) {
+        return this.renderLoadingView();
+    }
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+    },
+
+    renderLoadingView: function() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies...
+                </Text>
             </View>
-        </View>
-    );
-  }
+        );
+    },
+
+    renderMovie: function(movie) {
+         return (
+             <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+             </View>
+         );
+    }
 });
 
 var styles = StyleSheet.create({
